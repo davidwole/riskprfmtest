43368e28131ac12e943f67ba9c157d7c1102a70a
David Oluwole <david@Davids-MacBook-Air.local>
2024-01-06 07:42:26 +0100

    Styling adjustments

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -38,8 +38,13 @@ export default function Home() {
                 </div>
 
                 <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sagittis ex viverra consequat viverra. Pellentesque pellentesque malesuada libero, et vehicula elit. Mauris scelerisque magna in pretium viverra. Phasellus nec facilisis nunc, id consequat nibh. Nam elementum gravida pellentesque. Nullam sollicitudin lacus nisi, sed accumsan ligula euismod facilisis. Sed scelerisque pharetra efficitur. Nam euismod tellus nec consequat fringilla. Curabitur nec sapien ac velit pulvinar tempor et eget massa. Phasellus eu diam nunc. Nunc ut nulla accumsan, sodales odio laoreet, euismod augue. Donec cursus nibh eu posuere pretium. Vestibulum molestie suscipit mi. Aenean felis magna, gravida id libero a, aliquet vulputate turpis. Aenean sollicitudin libero a turpis condimentum maximus. Fusce auctor accumsan nulla sed ornare.
-Ut non sapien vestibulum lorem fermentum dapibus ac sed diam. Praesent eget venenatis ex. Proin tincidunt, eros ac egestas dictum, ipsum mi cursus ante, et consequat augue urna eu est. Quisque placerat metus vitae lorem tempus, id pharetra
+                Welcome to Risk PRFM, where automotive excellence meets unmatched customer service. At Risk PRFM, we take pride in being your trusted partner on the road. With a passion for precision and a commitment to quality, our skilled technicians bring decades of experience to every vehicle that rolls into our shop.
+
+<span className='hide_for_mobile'>At the heart of our service philosophy is a dedication to transparency and integrity. We believe in open communication, ensuring you understand every step of the repair or maintenance process. From routine oil changes to complex engine diagnostics, we handle it all with precision and care.
+
+Equipped with state-of-the-art technology, our facility is designed to meet the evolving needs of modern vehicles. We source top-quality parts and employ the latest industry practices to deliver reliable and efficient solutions.
+
+Choose Risk PRFM for a seamless automotive experience that goes beyond the ordinary. Your satisfaction and the health of your vehicle are our top priorities. Drive with confidence – drive with Risk PRFM.</span>
                 </p>
             </div>
             </Element>
@@ -49,46 +54,48 @@ Ut non sapien vestibulum lorem fermentum dapibus ac sed diam. Praesent eget vene
                 <div className='accordions'>
                     <div className='accordion'>
                         <div className='accordion_prompt' onClick={() => handleAccordionClick(0)}>                            
-                            <p>Lorem ipsum dolor sit amet, consectetu?</p>
+                            <p>How often should I get my car's oil changed?</p>
                             <p>+</p>
                         </div>
 
                         <div className={activeIndex != 0 ? 'accordion_answer accordion_hide' : 'accordion_answer'}>
-                            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sagittis ex viverra consequat viverra. Pellentesque pellentesque malesuada libero, et vehicula elit. Mauris scelerisque magna in pretium viverra.</p>
+                            <p>It is generally recommended to change your car's oil every 3,000 to 5,000 miles or as specified in your vehicle's owner's manual. Regular oil changes help maintain engine health and extend the life of your vehicle.</p>
                         </div>
                     </div>
                     
                     <div className='accordion'>
                         <div className='accordion_prompt' onClick={() => handleAccordionClick(1)}>                            
-                            <p>Lorem ipsum dolor sit amet, consectetu?</p>
+                            <p>What are the signs that my brakes may need servicing?</p>
                             <p>+</p>
                         </div>
 
                         <div className={activeIndex != 1 ? 'accordion_answer accordion_hide' : 'accordion_answer'}>
-                            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sagittis ex viverra consequat viverra. Pellentesque pellentesque malesuada libero, et vehicula elit. Mauris scelerisque magna in pretium viverra.</p>
+                            <p>Look out for warning signs such as squeaking or grinding noises, reduced braking performance, a soft brake pedal, or vibrations while braking. If you experience any of these issues, it's crucial to have your brakes inspected and serviced promptly.</p>
                         </div>
                     </div>
                 </div>
                 <div className='accordions'>
                     <div className='accordion'>
                         <div className='accordion_prompt' onClick={() => handleAccordionClick(2)}>                            
-                            <p>Lorem ipsum dolor sit amet, consectetu?</p>
+                            <p>How often should I rotate my tires?</p>
                             <p>+</p>
                         </div>
 
                         <div className={activeIndex != 2 ? 'accordion_answer accordion_hide' : 'accordion_answer'}>
-                            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sagittis ex viverra consequat viverra. Pellentesque pellentesque malesuada libero, et vehicula elit. Mauris scelerisque magna in pretium viverra.</p>
+                            <p>Tire rotation is recommended every 6,000 to 8,000 miles or as advised in your vehicle's manual. Regular tire rotation helps ensure even tire wear, extends tire life, and improves overall performance and handling.</p>
                         </div>
                     </div>
                     
                     <div className='accordion'>
                         <div className='accordion_prompt' onClick={() => handleAccordionClick(3)}>                            
-                            <p>Lorem ipsum dolor sit amet, consectetu?</p>
+                            <p>What should I do if my check engine light comes on?</p>
                             <p>+</p>
                         </div>
 
                         <div className={activeIndex != 3 ? 'accordion_answer accordion_hide' : 'accordion_answer'}>
-                            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent sagittis ex viverra consequat viverra. Pellentesque pellentesque malesuada libero, et vehicula elit. Mauris scelerisque magna in pretium viverra.</p>
+                            <p>If your check engine light illuminates, it's essential to have your vehicle inspected as soon as possible. Ignoring the warning could lead to more significant issues. Our technicians can perform a diagnostic scan to identify the problem and provide recommendations for necessary repairs.
+
+</p>
                         </div>
                     </div>
                 </div>

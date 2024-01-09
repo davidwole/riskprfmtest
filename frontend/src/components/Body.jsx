545266ba5f6ef6e61955ff3f77fabf5e4c1489e7
davidwole <davidwole20@gmail.com>
2024-01-09 07:46:24 +0100

    Update Body.jsx

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -23,13 +23,13 @@ export default function Body(){
     return(
        <div className='body'>
             <Routes>
-            <Route path='/' element={<Home/>} />
+            <Route path='/' element={user ? <Admin /> : <Home/>} />
             <Route path='/login' element={<Login />} />
             <Route path='/appointment' element={<Appointment />} />
             <Route path='/service' element={<Service />} />
             <Route path='/contact' element={<Contact />} />
             <Route path='/create' element={<CreateAccount />} />
-            <Route path='/admin' element={<Admin />} />
+{/*             <Route path='/admin' element={<Admin />} /> */}
             <Route path='/register/:token' element={<Register />} />
             <Route path='/*' element={user ? <Admin /> : <Home />} />
             </Routes>

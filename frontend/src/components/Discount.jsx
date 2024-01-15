9ab208aa1c7dea9666076c2d3fb4871010de716a
davidwole <davidwole20@gmail.com>
2024-01-15 06:29:42 +0100

    Create Discount.jsx

---
+++ frontend/src/components/Discount.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import '../styles/Discount.css'
+
+export default function Discount() {
+    const [discountMessage, setDiscountMessage] = useState();
+
+    const getDiscount = async () => {
+        const response = await fetch('http://localhost:8080/api/discount');
+        const json = await response.json();
+
+        setDiscountMessage(json[0].message);
+
+    }
+
+    useEffect(() => {
+        getDiscount();
+    }, [])
+
+    return(
+       <Link to='/appointment'>
+            <div className="discount">
+                <div className="marquee">
+                    <p className="discount_text">{ discountMessage }</p>
+                </div>
+            </div>
+       </Link>
+    )
+}

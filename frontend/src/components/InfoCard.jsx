66d3b3f32e388f4988a6f0f4446ffab8fea0641e
davidwole <davidwole20@gmail.com>
2024-01-15 22:06:04 +0100

    Update InfoCard.jsx

--- frontend/src/components/InfoCard.jsx
+++ frontend/src/components/InfoCard.jsx
@@ -4,8 +4,8 @@ export default function InfoCard() {
     return(
         <div className="info_card hide_for_mobile">
             <ul>
-                <li>(000)-0000-0000</li>
-                <li>name@email.com</li>
+                <li>(239)-766-0003</li>
+                <li>riskperformancellc@gmail.com</li>
                 <li>123 Main Street, New York, NY, 10001.</li>
             </ul>
         </div>

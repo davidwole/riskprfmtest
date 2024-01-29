fff7e4bdad0ae9e081885cca61306013eafd55e4
davidwole <davidwole20@gmail.com>
2024-01-29 16:04:07 +0100

    Update InfoCard.jsx

--- frontend/src/components/InfoCard.jsx
+++ frontend/src/components/InfoCard.jsx
@@ -5,7 +5,7 @@ export default function InfoCard() {
         <div className="info_card hide_for_mobile">
             <ul>
                 <li>(239)-766-0003</li>
-                <li>riskperformancellc@gmail.com</li>
+                <li>riskpeformancellc@gmail.com</li>
                 <li>4417 NE 14th Place, Cape Coral FL 33909.</li>
             </ul>
         </div>
